6ad248b2202448fc22fd204609b0a572bd4c809d
Carlo Paredes <c.nick.paredes@gmail.com>
2020-05-05 11:46:21 -0700

    Added header content type error check

--- api.js
+++ api.js
@@ -8,6 +8,12 @@ function listApiFetch(...args) {
             if(!res.ok) {
                 error = { code: res.status };
             }
+
+            if(!res.headers.get('content-type').includes('json')) {
+                error.message = res.statusText;
+                return Promise.reject(error);
+            }
+
             return res.json();
         })
         .then(data => {
